911a4aff48886aa27ee754ddc2124491f3b7a06b
Sharon Rosner <ciconia@gmail.com>
2019-08-21 10:00:41 +0200

    Improve packing algorithm
    - Put all sources (zipped) in DATA section
    - Move all bootstrap code into Modulation::Packing

--- lib/modulation/packing.rb
+++ lib/modulation/packing.rb
@@ -2,88 +2,72 @@
 
 require_relative '../modulation'
 require 'zlib'
-require 'digest/md5'
 
 module Modulation
   # Implements packing functionality
   module Packing
-    BOOTSTRAP = <<~SRC.encode('ASCII-8BIT')
+    BOOTSTRAP = <<~SRC.encode('ASCII-8BIT').gsub(/^\s+$/, '').gsub(/\n\n+/, "\n").chomp
       # encoding: ASCII-8BIT
-      require 'modulation'
-      require 'zlib'
-
-      b = Modulation::Builder
-      z = Zlib::Inflate
-      b::C = {%<module_dictionary>s}
-
-      class << b
-        alias_method :orig_make, :make
-
-        def make(info)
-          (pr = Modulation::Builder::C.delete(info[:location])) ? pr.call : orig_make(info)
-        end
-      end
-
+      require 'bundler/setup'
+      require 'modulation/packing'
+      
+      Modulation::Packing.setup_packed_app(DATA, %<dictionary>s)
+      
       import(%<entry_point>s).send(:main)
+      __END__
+      %<data>s
     SRC
-
-    MAKE_CODE = <<~SRC
-      proc { b.orig_make(location: %<location>s, source: %<source>s) }
-    SRC
-
-    UNZIP_CODE = 'z.inflate(%<data>s)'
-
-    # MAKE_CODE = <<~SRC
-    #   Modulation::Builder.orig_make(location: %s, source: %s)
-    # SRC
-
-    # UNCAN_CODE = <<~SRC
-    #   proc { RubyVM::InstructionSequence.load_from_binary(
-    #     Zlib::Inflate.inflate(%s)).eval
-    #   }
-    # SRC
 
     def self.pack(paths, _options = {})
       paths = [paths] unless paths.is_a?(Array)
 
       entry_point_filename = nil
+
+      last_offset = 0
+      data = (+'').encode('ASCII-8BIT')
       dictionary = paths.each_with_object({}) do |path, dict|
         warn "Processing #{path}"
-        source = IO.read(path)
+        zipped = Zlib::Deflate.deflate(IO.read(path))
         entry_point_filename ||= path
-        dict[path] = pack_module(path, source)
+        length = zipped.bytesize
+        dict[path] = [last_offset, length]
+        data << zipped
+        last_offset += length
       end
 
-      generate_bootstrap(dictionary, entry_point_filename)
+      generate_bootstrap(dictionary, data, entry_point_filename)
     end
 
-    def self.pack_module(path, source)
-      # code = (MAKE_CODE % [fn.inspect, module_source.inspect])
-      # seq = RubyVM::InstructionSequence.compile(code, options)
-      # canned = Zlib::Deflate.deflate(seq.to_binary)
-      # dict[fn] = UNCAN_CODE % canned.inspect
-
-      zipped = Zlib::Deflate.deflate(source)
-      code = format(UNZIP_CODE, data: zipped.inspect)
-      format(MAKE_CODE, location: path.inspect, source: code)
-    end
-
-    def self.generate_bootstrap(module_dictionary, entry_point)
+    def self.generate_bootstrap(dictionary, data, entry_point)
       format(
         BOOTSTRAP,
-        module_dictionary: format_module_dictionary(module_dictionary),
-        entry_point: entry_point.inspect
-      ).chomp.gsub(/^\s+/, '').gsub(/\n+/, "\n")
+        dictionary:   dictionary.inspect,
+        entry_point:  entry_point.inspect,
+        data:         data
+      )
     end
 
-    def self.format_module_dictionary(module_dictionary)
-      module_dictionary.map do |fn, code|
-        format(
-          '%<filename>s => %<code>s',
-          filename: fn.inspect,
-          code: code
-        ).chomp
-      end.join(',')
+    def self.setup_packed_app(data, dictionary)
+      Modulation::Builder.const_set(:DATA, data)
+      Modulation::Builder.const_set(:DATA_POS, data.pos)
+      Modulation::Builder.const_set(:DICTIONARY, dictionary)
+
+      class << Modulation::Builder
+        alias_method :orig_make, :make
+        def make(info)
+          if (data_info = Modulation::Builder::DICTIONARY.delete(info[:location]))
+            from_data(info[:location], *data_info)
+          else
+            orig_make(info)
+          end
+        end
+
+        def from_data(location, offset, length)
+          Modulation::Builder::DATA.seek(Modulation::Builder::DATA_POS + offset)
+          source = Zlib::Inflate.inflate(DATA.read(length))
+          orig_make(location: location, source: source)
+        end
+      end
     end
   end
 end
